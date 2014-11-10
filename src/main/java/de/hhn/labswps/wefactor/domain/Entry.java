dc1dc232b1857952f9f656e54ac74aaf1b9e82da
Patrick Wohlgemuth <patrick.wohlgemuth@wohlgem.de>
2014-11-10 12:52:30 +0100

    changes

--- src/main/java/de/hhn/labswps/wefactor/domain/Entry.java
+++ src/main/java/de/hhn/labswps/wefactor/domain/Entry.java
@@ -26,59 +26,8 @@ public final class Entry {
     /** The entry description. */
     private String entryDescription = null;
 
-    /**
-     * Sets the entry code text.
-     *
-     * @param entryCodeTextParam
-     *            the new entry code text
-     */
-    public void setEntryCodeText(final String entryCodeTextParam) {
-        this.entryCodeText = entryCodeTextParam;
-    }
-
     /** The id. */
     private Long id;
-
-    /**
-     * Gets the id.
-     *
-     * @return the id
-     */
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * Sets the id.
-     *
-     * @param idParam
-     *            the new id
-     */
-    public void setId(final Long idParam) {
-        this.id = idParam;
-    }
-
-    /**
-     * Sets the entry date.
-     *
-     * @param entyDate
-     *            the new entry date
-     */
-    public void setEntryDate(final Date entyDate) {
-        this.entryDate = entyDate;
-    }
-
-    /**
-     * Sets the entry description.
-     *
-     * @param entryDescriptionParam
-     *            the new entry description
-     */
-    public void setEntryDescription(final String entryDescriptionParam) {
-        this.entryDescription = entryDescriptionParam;
-    }
 
     /**
      * Gets the entry code text.
@@ -105,5 +54,56 @@ public final class Entry {
      */
     public String getEntryDescription() {
         return this.entryDescription;
+    }
+
+    /**
+     * Gets the id.
+     *
+     * @return the id
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Sets the entry code text.
+     *
+     * @param entryCodeTextParam
+     *            the new entry code text
+     */
+    public void setEntryCodeText(final String entryCodeTextParam) {
+        this.entryCodeText = entryCodeTextParam;
+    }
+
+    /**
+     * Sets the entry date.
+     *
+     * @param entyDate
+     *            the new entry date
+     */
+    public void setEntryDate(final Date entyDate) {
+        this.entryDate = entyDate;
+    }
+
+    /**
+     * Sets the entry description.
+     *
+     * @param entryDescriptionParam
+     *            the new entry description
+     */
+    public void setEntryDescription(final String entryDescriptionParam) {
+        this.entryDescription = entryDescriptionParam;
+    }
+
+    /**
+     * Sets the id.
+     *
+     * @param idParam
+     *            the new id
+     */
+    public void setId(final Long idParam) {
+        this.id = idParam;
     }
 }
