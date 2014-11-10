0c33829afaf5bf5cdbbe2dfdbba3cc47a79b30a2
Patrick Wohlgemuth <patrick.wohlgemuth@wohlgem.de>
2014-11-10 12:44:17 +0100

    added Entry

---
+++ src/main/java/de/hhn/labswps/wefactor/domain/EntryRepository.java
@@ -0,0 +1,10 @@
+package de.hhn.labswps.wefactor.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * The Interface EntryRepository.
+ */
+public interface EntryRepository extends CrudRepository<Entry, Long> {
+
+}
