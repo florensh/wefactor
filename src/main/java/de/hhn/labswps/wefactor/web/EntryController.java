74cd7f39b9426ba9424a245c6698daadc442bf01
Florens Hückstädt <fhueckst@stud.hs-heilbronn.de>
2014-11-11 15:32:31 +0100

    view profiles from any user, fix styles.css bug

--- src/main/java/de/hhn/labswps/wefactor/web/EntryController.java
+++ src/main/java/de/hhn/labswps/wefactor/web/EntryController.java
@@ -7,9 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import de.hhn.labswps.wefactor.domain.EntryRepository;
 import de.hhn.labswps.wefactor.web.DataObjects.EntriesFilterDataObject;
@@ -55,6 +57,12 @@ public class EntryController {
             final Principal currentUser, final Model model) {
 
         return "entryedit";
+    }
+
+    @RequestMapping(value = "/entry/details", method = RequestMethod.GET)
+    public String showEntryDetails(@RequestParam("id") int id, ModelMap model) {
+        System.out.println(id);
+        return "entrydetails";
     }
 
 }
--- src/main/java/de/hhn/labswps/wefactor/web/UserController.java
+++ src/main/java/de/hhn/labswps/wefactor/web/UserController.java
@@ -12,9 +12,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import de.hhn.labswps.wefactor.domain.UserProfile;
 import de.hhn.labswps.wefactor.domain.UserProfileRepository;
@@ -47,10 +49,10 @@ public class UserController {
         return "registration";
     }
 
-    @RequestMapping(value = "/user/profile", method = RequestMethod.GET)
-    public String showSettings(Model model) {
-        return "profile";
-    }
+    // @RequestMapping(value = "/user/profile", method = RequestMethod.GET)
+    // public String showSettings(Model model) {
+    // return "profile";
+    // }
 
     @RequestMapping(value = "/user/profile/edit", method = RequestMethod.GET)
     public String showEditProfilePage(Model model, Principal currentUser) {
@@ -112,6 +114,15 @@ public class UserController {
                         .getAuthorities()));
 
         return "redirect:/";
+    }
+
+    @RequestMapping(value = "/user/profile/details", method = RequestMethod.GET)
+    public String showUserProfile(@RequestParam("id") Long id, ModelMap model) {
+        System.out.println(id);
+
+        UserProfile profile = this.userProfileRepository.findOne(id);
+        model.addAttribute("profile", profile);
+        return "profile";
     }
 
 }
