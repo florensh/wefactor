8eebe88ee6cbaa941bd3143708041a455d2f1db2
Patrick Wohlgemuth <patrick.wohlgemuth@wohlgem.de>
2014-12-05 17:24:36 +0100

    Java Klasse für Stringbegrenzungen geschrieben "StringUtil"
    In dieser klasse können sämtliche Strings begrenzt werden an deren Ende dann 3 Punkte angefügt werden.
    Ebenso wurde ein TestCase erstellt. und alle Tests laufen auf grün.

---
+++ src/test/java/de/hhn/labswps/wefactor/web/util/StringUtilTest.java
@@ -0,0 +1,56 @@
+package de.hhn.labswps.wefactor.web.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import de.hhn.labswps.wefactor.BaseTest;
+
+/**
+ * @author Patrick Wohlgemuth
+ *         The Class StringUtilTest.
+ */
+public class StringUtilTest extends BaseTest {
+
+    @Autowired
+    private StringUtil stringUtil;
+
+    /**
+     * Test string cut.
+     */
+    @Test
+    public void CutStringTest() {
+        String s = "testEmailAdresse@test.de";
+        int i = 17;
+
+        String result = this.stringUtil.getCutString(s, i);
+        assertEquals("testEmailAdresse@...", result);
+    }
+
+    /**
+     * Test string uncutet
+     */
+    @Test
+    public void UncutetCutStringTest() {
+        String s = "testmail@test.de";
+        int i = 17;
+
+        String result = this.stringUtil.getCutString(s, i);
+        assertEquals("testmail@test.de", result);
+    }
+
+    /**
+     * Test string empty
+     */
+    @Test
+    public void EmptyStringTest() {
+        String s = "";
+        int i = 17;
+
+        String result = this.stringUtil.getCutString(s, i);
+        assertEquals("", result);
+
+    }
+
+}
