d6913492ec494be4288daccddd5f56a913457c19
Florens Hückstädt <fhueckst@stud.hs-heilbronn.de>
2015-05-09 12:08:13 +0200

    testdata for diff with one version

--- src/main/java/de/hhn/labswps/wefactor/service/util/TestDataUtil.java
+++ src/main/java/de/hhn/labswps/wefactor/service/util/TestDataUtil.java
@@ -1,11 +1,14 @@
 package de.hhn.labswps.wefactor.service.util;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+
+import org.apache.commons.io.IOUtils;
 
 import de.hhn.labswps.wefactor.domain.Account;
 import de.hhn.labswps.wefactor.domain.AccountRepository;
@@ -22,6 +25,8 @@ import de.hhn.labswps.wefactor.domain.TimelineEvent;
 import de.hhn.labswps.wefactor.domain.TimelineEventRepository;
 import de.hhn.labswps.wefactor.domain.UserProfile;
 import de.hhn.labswps.wefactor.domain.UserProfileRepository;
+import de.hhn.labswps.wefactor.domain.VersionEntry;
+import de.hhn.labswps.wefactor.domain.VersionEntryRepository;
 import de.hhn.labswps.wefactor.specification.WeFactorValues;
 import de.hhn.labswps.wefactor.specification.WeFactorValues.EventType;
 import de.hhn.labswps.wefactor.specification.WeFactorValues.ProgrammingLanguage;
@@ -62,6 +67,8 @@ public class TestDataUtil {
     /** The master entry repository. */
     private MasterEntryRepository masterEntryRepository;
 
+    private VersionEntryRepository versionEntryRepository;
+
     /** The account repository. */
     private AccountRepository accountRepository;
 
@@ -74,7 +81,8 @@ public class TestDataUtil {
             MasterEntryRepository masterEntryRepository,
             TagRepository tagRepository,
             UserProfileRepository userProfileRepository,
-            TimelineEventRepository timelineEventRepository) {
+            TimelineEventRepository timelineEventRepository,
+            VersionEntryRepository versionEntryRepository) {
 
         this.groupRepository = groupRepository;
         this.timelineEventRepository = timelineEventRepository;
@@ -83,6 +91,7 @@ public class TestDataUtil {
         this.masterEntryRepository = masterEntryRepository;
         this.accountRepository = accountRepository;
         this.userProfileRepository = userProfileRepository;
+        this.versionEntryRepository = versionEntryRepository;
 
     }
 
@@ -96,10 +105,10 @@ public class TestDataUtil {
 
         generateGroups();
         generateUser();
-        generateEntries();
-        generateProposals();
-        generateVersions();
-        generateEntryRankings();
+        // generateEntries();
+        // generateProposals();
+        // generateVersions();
+        // generateEntryRankings();
         generateTestEntryForDiff();
 
     }
@@ -118,25 +127,56 @@ public class TestDataUtil {
         tag2.setName("JavaFX");
         tag2 = this.tagRepository.save(tag2);
 
-        ProgrammingLanguage[] langs = WeFactorValues.ProgrammingLanguage
-                .values();
-
         MasterEntry e = new MasterEntry();
-        e.setName("Diff Test");
-        e.setEntryDescription(ENTRY_DESCRIPTION);
-        e.setEntryCodeText("sf");
-        e.setEntryDate(new Date(System.currentTimeMillis()));
+        e.setName("Asynchrone Aufrufe  in JavaFX (Diff Test)");
+        e.setEntryDescription("<p>Hier ein Beispiel, wie man die Manipulation von Attributen im GVS-Projekt &uuml;ber einen asynchronen Aufrufmechanismus abhandeln kann. Die hier dargestellte Task ist f&uuml;r mehrere Aufrufe, n&auml;mlich f&uuml;r alle Aufrufe, die sich mit dem Manipulieren von Attributen besch&auml;ftigt, konfiguriert. Die Aufrufe selbst werden &uuml;ber eine \"Kernel\"-Klasse durchgef&uuml;hrt. Das Aufrufergebnis wird einem \"Identity\"-Objekt &uuml;bergeben, welches dann von der GUI visualisiert wird.</p>");
+
+        String content = null;
+        try {
+            FileInputStream stream = new FileInputStream(
+                    "src/main/resources/testdata/DiffTest.txt");
+            content = IOUtils.toString(stream, "utf-8");
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        e.setEntryCodeText(content);
+        e.setEntryDate(new Date(System.currentTimeMillis() - 1000));
         e.setAccount(up.getAccount());
         e.setLanguage(WeFactorValues.ProgrammingLanguage.Java.getMode());
-        e.setTeaser(ENTRY_TEASER);
-        EntryRating rating = new EntryRating();
-        rating.setAccount(up.getAccount());
-        rating.setValue((int) (Math.random() * 5));
-        e.addRating(rating);
+        e.setTeaser("Auch in JavaFX sollte man Aufrufe unbestimmter Laufzeit in separate Threads auslagern. Dazu bietet JavaFX die Verwendung der Klasse Task an...");
         e.addTag(tag1);
         e.addTag(tag2);
         this.masterEntryRepository.save(e);
-        this.entryRatingRepository.save(rating);
+
+        VersionEntry version = new VersionEntry();
+        version.setMasterOfVersion(e);
+        version.setAccount(e.getAccount());
+        version.setChanges(e.getChanges());
+
+        try {
+            FileInputStream stream = new FileInputStream(
+                    "src/main/resources/testdata/DiffTest_Version.txt");
+            content = IOUtils.toString(stream, "utf-8");
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        version.setEntryCodeText(content);
+        version.setEntryDate(new Date(System.currentTimeMillis()));
+        version.setEntryDescription(e.getEntryDescription());
+        version.setLanguage(e.getLanguage());
+        version.setName(e.getName());
+        version.setTeaser(e.getTeaser());
+        version.setChanges("some changes to test the diff view!");
+
+        if (!e.getTags().isEmpty()) {
+            for (final Tag tag : e.getTags()) {
+                version.addTag(tag);
+            }
+        }
+
+        this.versionEntryRepository.save(version);
 
     }
 
